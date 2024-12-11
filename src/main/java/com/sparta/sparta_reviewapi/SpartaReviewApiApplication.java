f84970ed38f41b686d08d85ed6220fdcf2b2a007
Choi-Hyeok99 <gur0709@naver.com>
2024-12-11 10:30:45 +0900

    init

---
+++ src/main/java/com/sparta/sparta_reviewapi/SpartaReviewApiApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.sparta_reviewapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaReviewApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaReviewApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/sparta_reviewapi/SpartaReviewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.sparta_reviewapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaReviewApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
