cc02198049b90158b01409c98d2ce0c0a08d8792
Choi-Hyeok99 <gur0709@naver.com>
2024-12-11 13:07:18 +0900

    Update : interface ReviewRepository Jpa 적용

--- src/main/java/com/sparta/sparta_reviewapi/review/repository/ReviewRepository.java
+++ src/main/java/com/sparta/sparta_reviewapi/review/repository/ReviewRepository.java
@@ -1,7 +1,9 @@
 package com.sparta.sparta_reviewapi.review.repository;
 
+import com.sparta.sparta_reviewapi.review.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ReviewRepository {
+public interface ReviewRepository extends JpaRepository<Review,Long> {
 }
