f890474afcbb5bf78f9aca27d87a2e97c10a6e9c
Choi-Hyeok99 <gur0709@naver.com>
2024-12-11 12:48:26 +0900

    Create : ReviewController

---
+++ src/main/java/com/sparta/sparta_reviewapi/review/controller/ReviewController.java
@@ -0,0 +1,23 @@
+package com.sparta.sparta_reviewapi.review.controller;
+
+import com.sparta.sparta_reviewapi.review.entity.ReviewRequestDto;
+import com.sparta.sparta_reviewapi.review.service.ReviewService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/products/{productId}/reviews") // 상품별 리뷰 API
+public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    // 리뷰 등록
+    @PostMapping
+    public String CreateReview(@PathVariable Long productId,
+                               @RequestBody ReviewRequestDto requestDto){
+       return reviewService.createReview(productId,requestDto);
+    }
+
+    // 리뷰 조회
+}
