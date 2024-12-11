5092f6f753b2a3260eae75c151a0acde6a54726c
Choi-Hyeok99 <gur0709@naver.com>
2024-12-11 21:55:09 +0900

    Update : controller 수정

--- src/main/java/com/sparta/sparta_reviewapi/review/controller/ReviewController.java
+++ src/main/java/com/sparta/sparta_reviewapi/review/controller/ReviewController.java
@@ -1,9 +1,9 @@
 package com.sparta.sparta_reviewapi.review.controller;
 
 import com.sparta.sparta_reviewapi.review.entity.ReviewRequestDto;
+import com.sparta.sparta_reviewapi.review.entity.ReviewResponseDto;
 import com.sparta.sparta_reviewapi.review.service.ReviewService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,10 +15,10 @@ public class ReviewController {
 
     // 리뷰 등록
     @PostMapping
-    public ResponseEntity<String> CreateReview(@PathVariable Long productId,
-                                       @RequestBody ReviewRequestDto requestDto){
-       reviewService.createReview(productId,requestDto);
-        return ResponseEntity.ok("ok");
+    public ReviewResponseDto createReview(@PathVariable("productId") Long productId,
+                                          @ModelAttribute ReviewRequestDto requestDto){
+
+        return reviewService.createReview(productId,requestDto);
     }
 
     // 리뷰 조회
