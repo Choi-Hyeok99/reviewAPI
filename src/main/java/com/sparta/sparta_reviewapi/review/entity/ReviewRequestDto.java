b23ed5714696e3f20a56320b87496ffba2103359
Choi-Hyeok99 <gur0709@naver.com>
2024-12-11 21:54:27 +0900

    Update : Multipart 필드 추가

--- src/main/java/com/sparta/sparta_reviewapi/review/entity/ReviewRequestDto.java
+++ src/main/java/com/sparta/sparta_reviewapi/review/entity/ReviewRequestDto.java
@@ -1,13 +1,14 @@
 package com.sparta.sparta_reviewapi.review.entity;
 
 import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
 
 @Data
 public class ReviewRequestDto {
 
-    private Long userId;
-    private Long productId;
-    private int score;
-    private String content;
+    private Long userId;      // 작성자 ID
+    private float score;      // 리뷰 점수 (1-5점)
+    private String content;   // 리뷰 내용
+    private MultipartFile image;  // MultipartFile 필드
 
 }
